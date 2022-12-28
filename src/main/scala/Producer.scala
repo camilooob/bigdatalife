1bd260e0e0eb7db72ae77ac14081c3f578d1140b
Camilo Baquero <camilobaq@hotmail.com>
2022-12-28 01:59:16 -0500

    Update Producer.scala

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -13,7 +13,6 @@
   * See the License for the specific language governing permissions and
   * limitations under the License.
   */
-package io.confluent.examples.clients.scala
 
 import java.io.FileReader
 import java.util.Properties
